0a18ff5031f0d0dcfd47e75faaa964ad438632ff
Sonali Kumari <astutesona@gmail.com>
2026-08-08 11:40:31 +0530

    Implement method to find the largest element in a list

---
+++ largestElement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestElement(self,nums):
+
+        largest=nums[0]
+        
+        for i in range(1,len(nums)):
+            if nums[i]>largest:
+                largest=nums[i]
+
+                return largest
